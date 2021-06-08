bcd5e5f0cf33fa9597c7a10adf802c0d429be826
Davide <daltodavide@gmail.com>
2021-06-08 21:15:51 +0100

    fix

--- StringBuilderInHashMap.java
+++ StringBuilderInHashMap.java
@@ -1,7 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 // //DEPS <dependency1> <dependency2>
-// JAVA 16
+//JAVA 16
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,8 +12,8 @@ import static java.lang.System.*;
 public class StringBuilderInHashMap {
 
     public static void main(String... args) {
-        List<StringBuilder> list = null; //...
-        StringBuilder sb = null; // ...
+        List<StringBuilder> list = new ArrayList<>(); //...
+        StringBuilder sb = new StringBuilder(); // ...
         Set<StringBuilder> set = new HashSet<>( list );
         set.add( sb );
         out.println( set.contains( sb ) );
