c99c8f801c18db8d9260c427712bf12ccf9254a0
Davide <daltodavide@gmail.com>
2021-06-08 21:28:10 +0100

    fix

--- StringBuilderInHashMap.java
+++ StringBuilderInHashMap.java
@@ -6,14 +6,26 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.*;
 
 import static java.lang.System.*;
 
+// Replace '...' in this code with proper Java expressions so that the first println prints 'true'
+// and the second one prints 'false' when running on Java 16.
+// This is a fair puzzle:
+//        - No reflection;
+//        - No hacking the output stream;
+//        - No unchecked code (e. g., List<StringBuilder> contains StringBuilder objects only);
+//        - No hidden replacement of library classes (List is standard java.util.List, Set is java.util.Set, etc.).
 public class StringBuilderInHashMap {
 
     public static void main(String... args) {
-        List<StringBuilder> list = new ArrayList<>(); //...
-        StringBuilder sb = new StringBuilder(); // ...
+        // The real code should be:
+        // List<StringBuilder> list = ...;
+        // StringBuilder sb = ...;
+        List<StringBuilder> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
         Set<StringBuilder> set = new HashSet<>( list );
         set.add( sb );
         out.println( set.contains( sb ) );
